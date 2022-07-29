fb76a8d6d9b54e91ae842a9dd850a200d0ccc259
Optifun <frdima19@gmail.com>
2022-07-29 20:31:27 +0700

    Update ChatClient: fix url & add events

--- backend/Chat.Client/ChatClient.cs
+++ backend/Chat.Client/ChatClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
@@ -9,29 +10,50 @@ using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Chat.Client
 {
-    public class ChatClient
+    public class ChatClient : IAsyncDisposable
     {
         private readonly HubConnection _connection;
 
         public event Action<List<MessageDTO>> LastMessagesReceived;
         public event Action<MessageDTO> MessageReceived;
+        public event Func<Exception, Task> Closed;
+        public event Func<string, Task> Reconnected;
+        public event Func<Exception, Task> Reconnecting;
 
         public ChatClient(IPAddress address)
         {
-            _connection = new HubConnectionBuilder().WithUrl($"ws://{address}:8081/ws/chat").Build();
+            _connection = new HubConnectionBuilder().WithUrl($"http://{address}:8081/ws/chat").Build();
 
+            _connection.Closed += OnClosed;
+            _connection.Reconnected += OnReconnected;
+            _connection.Reconnecting += OnReconnecting;
             _connection.On(WSMessage.ReceiveLast10.ToString(), (IEnumerable<MessageDTO> messages) => { LastMessagesReceived?.Invoke(messages.ToList()); });
 
             _connection.On(WSMessage.Receive.ToString(), (MessageDTO message) => { MessageReceived?.Invoke(message); });
         }
 
-        public Task Connect() => 
+        public Task Connect() =>
             _connection.StartAsync();
 
-        public Task SendMessageAsync(WSMessage messageType, object arg) => 
+        public Task SendMessageAsync(WSMessage messageType, object arg) =>
             _connection.SendAsync(messageType.ToString(), arg);
 
-        public Task Disconnect() => 
+        public Task Disconnect() =>
             _connection.StopAsync();
+
+        private Task OnReconnecting(Exception ex) =>
+            Reconnecting?.Invoke(ex);
+
+        private Task OnReconnected(string connectionId) =>
+            Reconnected?.Invoke(connectionId);
+
+        private Task OnClosed(Exception ex) =>
+            Closed?.Invoke(ex);
+
+        public ValueTask DisposeAsync()
+        {
+            Disconnect();
+            return _connection.DisposeAsync();
+        }
     }
 }
