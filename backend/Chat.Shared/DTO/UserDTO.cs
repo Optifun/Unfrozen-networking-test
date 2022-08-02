d2b6776fa61c4c1b9275c3e73b3dd10e25ed3b07
Optifun <frdima19@gmail.com>
2022-08-02 16:34:26 +0700

    Update UserDTO: add equality members

--- backend/Chat.Shared/DTO/UserDTO.cs
+++ backend/Chat.Shared/DTO/UserDTO.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace Chat.Shared.DTO
 {
-    public class UserDTO
+    public class UserDTO : IEquatable<UserDTO>
     {
         public long Color { get; }
 
@@ -11,5 +13,34 @@ namespace Chat.Shared.DTO
             Color = color;
             Name = name;
         }
+
+        public bool Equals(UserDTO other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Color == other.Color && Name == other.Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((UserDTO) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Color.GetHashCode() * 397) ^ Name.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(UserDTO left, UserDTO right) => 
+            Equals(left, right);
+
+        public static bool operator !=(UserDTO left, UserDTO right) => 
+        !Equals(left, right);
     }
 }
